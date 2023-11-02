d892a4f4cd5e31dcf4c9ec8670f4dec8f9244341
Woochang1132 <show4031132@gmail.com>
2023-11-02 12:01:03 +0900

    최우창 react_shoppy 챌린지 : firebase build setup 및 로그인

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {AiOutlineShopping} from 'react-icons/ai'
 import {BsFillPencilFill} from 'react-icons/bs'
 import { Link } from 'react-router-dom';
+import { login } from '../api/firebase';
 
 export default function Navbar() {
+    const {logincheck} = useState(false);
+
     return (
         <header className='flex justify-between border-b border-gray-300 p-2'>
             <Link to='/' className='flex items-center text-4xl text-brand'>
@@ -16,7 +19,11 @@ export default function Navbar() {
                 <Link to='/products/new' className='text-2xl'>
                     <BsFillPencilFill/>
                 </Link>
-                <button>Login</button>
+                <button onClick={login}>{logincheck ? 'Logout' : 'Login'}</button>
+                {/* 인자와 호출하는 인자가 동일하면 생락 가능하다. 
+                    () => login() 
+                    login 다음과 같이 생략이 가능하다.
+                */}
             </nav>
         </header>
     );

0dbcaeec1566c4bb7a596b315c32ba372b56c8e8
Woochang1132 <show4031132@gmail.com>
2023-11-07 12:40:07 +0900

    shoppy 챌린지 => navigate를 활용한 상세페이지 적용

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 
-export default function ProductCard({product: {id, image ,title, category, price}}) {
+export default function ProductCard({
+    product,
+    product: {id, image ,title, category, price}}) {
+    const navigate = new useNavigate();
     return (
-        <li className='rounded-lg shadow-md overflow-hidden cursor-pointer'>
+        <li onClick={() => {
+            navigate(`/products/${id}`, {state: {product: product}})
+        }} className='rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105'>
            <img className='w-full ' src={image} alt={title} />
             <div className='mt-2 px-2 text-lg flex justify-between itmes-center'>
                 <h3 className='truncate'>{title}</h3>
--- src/component/User.jsx
+++ src/component/User.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function User({user: {photoURL, displayName}}) {
-    return (
+        return (
         <div className='flex items-center shrink-0'>
             <img className='w-10 h-10 rounded-full mr-2' src={photoURL} alt={displayName}></img>
             <span className='hidden md:block'>{displayName}</span>

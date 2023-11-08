53b562d2cc8c3c07e5e59b4350c51ede80cb5c60
Woochang1132 <show4031132@gmail.com>
2023-11-08 11:50:24 +0900

    shoppy 챌린지 => 카트 페이지 구성하기, 기능 추가

---
+++ src/component/CartItem.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {RiDeleteBin5Fill} from 'react-icons/ri'
+import { AiOutlinePlusSquare, AiOutlineMinusSquare } from 'react-icons/ai';
+import { addOrUpdateToCart, removeFromCart } from '../api/firebase';
+
+export default function CartItem({product, 
+    product: {id, image, title, option, quantity, price}, uid}) {
+        const handleMinus = () => {
+            if(quantity < 2) return;
+            addOrUpdateToCart(uid, {...product, quantity: quantity - 1})
+        }
+        const handlePlus = () => {
+            if(quantity > 99) return;
+            addOrUpdateToCart(uid, {...product, quantity: quantity + 1})
+        }
+        const handleDelete = () => {
+            if(product.length < 1) return;
+            removeFromCart(uid, id);
+        }
+    return (
+        <li>
+            <img src={image} alt={title} />
+            <div>
+                <p>{title}</p>
+                <p>{option}</p>
+            </div>
+            <div>
+            <AiOutlineMinusSquare onClick={handleMinus}/>
+            <span>{quantity}</span>
+            <AiOutlinePlusSquare onClick={handlePlus}/>
+            <RiDeleteBin5Fill onClick={handleDelete}/>
+            </div>
+        </li>
+    );
+}
+
--- src/page/MyCart.jsx
+++ src/page/MyCart.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import { useAuthContext } from '../component/AuthContext';
+import { useQuery } from '@tanstack/react-query';
+import { getCart } from '../api/firebase';
+import CartItem from '../component/CartItem';
+import Price from '../component/Price';
+import { BsFillPlusCircleFill } from 'react-icons/bs';
+import {FaEquals} from 'react-icons/fa'
 
+const SHIPPING = 3000;
 export default function MyCart() {
+    const {uid} = useAuthContext();
+    const {isLoading , data: products } = useQuery({
+        queryKey: ['carts'],
+        queryFn: () => getCart(uid)
+    })
+    if(isLoading) return <p>Loding...</p>
+
+    const hasProducts = products && products.length > 0;
+    const totalPrice = products && products.reduce((prev, current) => prev + parseInt(current.price) * current.quantity, 0)
     return (
-        <div>
-            MyCart
-        </div>
+        <section>
+            <p>내 장바구니</p>
+            {!hasProducts && <p>장바구니에 상품이 없습니다.</p>}
+            {hasProducts && <>
+                <ul>
+                    {products && products.map((product) => (
+                        <CartItem key={product.id} product={product} uid={uid}></CartItem>
+                    ))}
+                </ul>
+                <div>
+                    <Price text="상품 총액" price={totalPrice}/>
+                    <BsFillPlusCircleFill/>
+                    <Price text="배송액" price={SHIPPING}/>
+                    <FaEquals/>
+                    <Price text='총가격' price={totalPrice + SHIPPING}/>
+                </div>
+            </>}
+        </section>
     );
 }
 
